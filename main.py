7a5680de8f34743136f56648b4d4ea5d33481405
OmellMwanza <omellmwanza55@gmail.com>
2026-06-14 20:52:21 +0200

    fixed some broken comments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
-load_dotenv()  # Load environment variables from .env file
+load_dotenv()  # Load environment variables from .env file.
 
 messages = []
 
